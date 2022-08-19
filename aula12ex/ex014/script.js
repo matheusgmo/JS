86ac9196c48a615ecb33c3dad8010c9852f50677
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2022-08-19 13:43:55 -0300

    ex014

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora>=0 && hora<12){
+        //bom dia
+        img.src = '../img/manhã.png'
+    }
+    else if(hora>=12 && hora<18){
+        //boa tarde
+        img.src = '../img/tarde.png'
+        document.body.style.background = '#A69D79'
+    }
+    else{
+        //boa noite
+        img.src = '../img/noite.png'
+        document.body.style.background = '#055951'
+    }
+}
+
