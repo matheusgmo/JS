85e4ba1626cbb040d9e638d5e2e323ab95cb72c8
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2022-11-29 11:54:00 -0300

    att1

---
+++ exercicio js/fundamentos/comentario.js
@@ -0,0 +1,14 @@
+//Comentário de 1 linha
+console.log('Linha 1')
+
+/*
+Comentário de
+Múltiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+ * Comentário de
+ * Múltiplas linhas
+ */
+console.log('Linha 3')
