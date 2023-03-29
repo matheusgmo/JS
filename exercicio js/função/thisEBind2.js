4165d7c988454047feb83a2162c526d920d70f16
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2023-03-29 14:00:20 -0300

    29/03 1

--- exercicio js/função/thisEBind2.js
+++ exercicio js/função/thisEBind2.js
@@ -1,10 +1,11 @@
 function Pessoa(){
     this.idade = 0
-
-    setInterval(function() {
-        this.idade++
-        console.log(this.idade)
-    }, 1000)
+    
+    const self = this
+    setInterval(function(){
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)
 }
 
-new Pessoa 
+new Pessoa
