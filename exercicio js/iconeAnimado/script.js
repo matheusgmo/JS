fde72d3a88cbb9ceba83cdff2a0d335ccabb7068
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2023-05-02 14:22:28 -0300

    iconeAnimado2

--- exercicio js/iconeAnimado/script.js
+++ exercicio js/iconeAnimado/script.js
@@ -1,18 +1,22 @@
-let checkbox = document.getElementById('checkbox')
-
-checkbox.addEventListener('change', function(){
-    let menu = document.getElementById('menu')
-    if(menu.classList.contains('check')){
-        menu.classList.remove('check')
-    } else{
-        menu.classList.add('check')
+let iconeMenu = document.getElementById('iconeMenu')
+let barra1 = document.getElementById('barra1')
+let barra2 = document.getElementById('barra2')
+let barra3 = document.getElementById('barra3')
+let menu = document.getElementById('menu')
+iconeMenu.addEventListener('click', function(){
+    if(iconeMenu.classList.contains('active')){
+        iconeMenu.classList.remove('active')
+        barra1.classList.remove('active')
+        barra2.classList.remove('active')
+        barra3.classList.remove('active')
+        menu.classList.remove('active')
+        .classList.remove('active')
+    }else{
+        iconeMenu.classList.add('active')
+        barra1.classList.add('active')
+        barra2.classList.add('active')
+        barra3.classList.add('active')
+        menu.classList.add('active')
+        .classList.add('active')
     }
 })
-
-let header = document.getElementsByTagName(header)
-
-function abrirMenu(){
-    if(menu.classList.contains('check')){
-        header.classList.add('aberto')
-    } else{}
-}
