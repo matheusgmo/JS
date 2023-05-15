9d784a45e6acdade6cf47975168d0339d251a037
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2023-05-15 11:53:33 -0300

    15/05 11:53

--- exercicio js/iconeAnimado/script.js
+++ exercicio js/iconeAnimado/script.js
@@ -1,22 +1,12 @@
-let iconeMenu = document.getElementById('iconeMenu')
-let barra1 = document.getElementById('barra1')
-let barra2 = document.getElementById('barra2')
-let barra3 = document.getElementById('barra3')
-let menu = document.getElementById('menu')
-iconeMenu.addEventListener('click', function(){
+const iconeMenu = document.getElementById('iconeMenu')
+const menu = document.getElementById('menu')
+
+iconeMenu.addEventListener('click', () => {
     if(iconeMenu.classList.contains('active')){
         iconeMenu.classList.remove('active')
-        barra1.classList.remove('active')
-        barra2.classList.remove('active')
-        barra3.classList.remove('active')
         menu.classList.remove('active')
-        .classList.remove('active')
     }else{
         iconeMenu.classList.add('active')
-        barra1.classList.add('active')
-        barra2.classList.add('active')
-        barra3.classList.add('active')
         menu.classList.add('active')
-        .classList.add('active')
     }
 })
